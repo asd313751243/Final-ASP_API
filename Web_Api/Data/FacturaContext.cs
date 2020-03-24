2401befa291c3fd7552b3f2450e6f0443fd44172
asd313751243 <asd313751243@gmail.com>
2020-03-24 17:56:40 -0400

    Conectando a la base de dato

---
+++ Web_Api/Data/FacturaContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Api.Models;
+
+namespace Web_Api.Data
+{
+    public class FacturaContext: DbContext
+    {
+        public FacturaContext(DbContextOptions<FacturaContext> options): base(options)
+        {
+        }
+
+        public DbSet<Almacen> Almacenes { get; set; }
+        public DbSet<Almacen_Producto> Almacenes_Productos { get; set; }
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Empleado_Producto> Empleados_Productos { get; set; }
+        public DbSet<Sucursal> Sucursales { get; set; }
+        public DbSet<Sucursal_Producto> Sucursales_Productos { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ Web_Api/Migrations/20200324214801_MigracionInicial.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web_Api.Migrations
+{
+    public partial class MigracionInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Almacenes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre_Almacen = table.Column<string>(nullable: true),
+                    Ciudad_Almacen = table.Column<string>(nullable: true),
+                    Telefono_Almacen = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Almacenes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Almacenes_Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Id_Almacen = table.Column<int>(nullable: false),
+                    Id_Producto = table.Column<int>(nullable: false),
+                    Cantidad_Almacen_Producto = table.Column<int>(nullable: false),
+                    Fecha_Almacen_Producto = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Almacenes_Productos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empleados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre_Empleado = table.Column<int>(nullable: false),
+                    Sexo_Empleado = table.Column<int>(nullable: false),
+                    Cedula_Empleado = table.Column<int>(nullable: false),
+                    Fecha_Nac_Empleado = table.Column<DateTime>(nullable: false),
+                    Telefono_Empleado = table.Column<int>(nullable: false),
+                    Usuario_Empleado = table.Column<string>(nullable: true),
+                    Contra_Empleado = table.Column<string>(nullable: true),
+                    Id_Fk_Sucursal = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleados", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empleados_Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Id_Empleado = table.Column<int>(nullable: false),
+                    Id_Producto = table.Column<int>(nullable: false),
+                    Fecha_Empleado_Producto = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleados_Productos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre_Producto = table.Column<string>(nullable: true),
+                    Precio_Producto = table.Column<double>(nullable: false),
+                    Fecha_Venc_Producto = table.Column<DateTime>(nullable: false),
+                    Cantidad = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sucursales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre_Sucursal = table.Column<string>(nullable: true),
+                    Ciudad_Sucursal = table.Column<string>(nullable: true),
+                    Telefono_Sucursal = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sucursales", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sucursales_Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Id_Sucursal = table.Column<int>(nullable: false),
+                    Id_Producto = table.Column<int>(nullable: false),
+                    Cantidad_Sucursal_Producto = table.Column<int>(nullable: false),
+                    Fecha_Sucursal_Producto = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sucursales_Productos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Almacenes");
+
+            migrationBuilder.DropTable(
+                name: "Almacenes_Productos");
+
+            migrationBuilder.DropTable(
+                name: "Empleados");
+
+            migrationBuilder.DropTable(
+                name: "Empleados_Productos");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Sucursales");
+
+            migrationBuilder.DropTable(
+                name: "Sucursales_Productos");
+        }
+    }
+}
